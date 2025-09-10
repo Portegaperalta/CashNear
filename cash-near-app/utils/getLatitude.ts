a51788a462d8a1591c580376562c139b6ee81a1e
Pablo <ortegaperaltap@gmail.com>
2025-09-09 20:20:12 -0400

    explicitly return undefined in getLonditude and getLatitude function in case the browser does not support geolocation

--- cash-near-app/utils/getLatitude.ts
+++ cash-near-app/utils/getLatitude.ts
@@ -1,6 +1,7 @@
 export default async function getLatitude(): Promise<number | undefined> {
   if (!("geolocation" in navigator)) {
     console.error("Error: geolocation is not supported by browser")
+    return undefined
   } else {
     return new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(
