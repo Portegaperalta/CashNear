39c131c0d1895f42977fae41cc6c6a83f134bb3b
Pablo <ortegaperaltap@gmail.com>
2025-09-09 22:30:43 -0400

    create function to handle form submission and prevent defaul when submitting search form

--- cash-near-app/components/SearchForm.tsx
+++ cash-near-app/components/SearchForm.tsx
@@ -1,9 +1,18 @@
+"use client"
+
 import { Search } from "lucide-react"
 
 export default function SearchForm() {
+
+  const handleFormSubmission = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+  }
+
   return (
     <div className="search-form">
-      <form className="px-2 flex flex-row-reverse gap-2 items-center bg-(--clr-white) rounded-md">
+      <form
+        onSubmit={handleFormSubmission}
+        className="px-2 flex flex-row-reverse gap-2 items-center bg-(--clr-white) rounded-md">
         <input
           type="text"
           placeholder="City,Zip code or Address"
