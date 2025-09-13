847c20058687e9f9c1de1745476afd4815ce8ff7
Pablo <ortegaperaltap@gmail.com>
2025-09-13 18:55:16 -0400

    wrap body content inside APIProvider and remove ssr from home page

--- cash-near-app/app/page.tsx
+++ cash-near-app/app/page.tsx
@@ -1,15 +1,22 @@
+"use client"
+
 import MapContent from "@/components/MapContent";
 import Header from "@/components/Header";
+import { APIProvider } from "@vis.gl/react-google-maps";
+
+const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
 
 export default function Home() {
   return (
     <div className="body-content">
-      <Header />
-      <main className="px-4 py-4">
-        <section className="home">
-          <MapContent />
-        </section>
-      </main>
+      <APIProvider apiKey={googleMapsApiKey!}>
+        <Header />
+        <main className="px-4 py-4">
+          <section className="map">
+            <MapContent />
+          </section>
+        </main>
+      </APIProvider>
     </div>
   );
 }
