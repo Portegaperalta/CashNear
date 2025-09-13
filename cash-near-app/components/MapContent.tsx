6f45d0842ab15ed95bab96eef97fa0083d1b4caf
Pablo <ortegaperaltap@gmail.com>
2025-09-13 17:37:15 -0400

    implement Google Maps Autocomplete feature and handle incompatible API versions in MapContent component

--- cash-near-app/components/MapContent.tsx
+++ cash-near-app/components/MapContent.tsx
@@ -1,16 +1,46 @@
 "use client"
 
-import { APIProvider, Map } from "@vis.gl/react-google-maps"
+import { useEffect, useState } from "react"
+import { APIProvider, Map, ControlPosition } from "@vis.gl/react-google-maps"
 import getLatitude from "@/utils/getLatitude"
 import getLongitude from "@/utils/getLongitude"
-import { useEffect, useState } from "react"
+import AutocompleteControl from "./react-google-maps/AutocompleteControl"
+import AutocompleteResult from "./react-google-maps/AutocompleteResult"
 
 const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
+const googleMapID = process.env.NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID
+
+export type AutocompleteMode = { id: string; label: string };
+
+const implementations: Array<AutocompleteMode> = [
+  { id: 'custom', label: 'Minimal Custom Build' },
+];
 
 export default function MapContent() {
-  const [currentLat, setCurrentLat] = useState<number | undefined>()
-  const [currentLong, setCurrentLong] = useState<number | undefined>()
+  // visgl google maps autocomplete state mangement
+  const [selectedImplementation, setSelectedImplementation] = useState<AutocompleteMode>(implementations[0]);
 
+  //current latitude and longitude state management
+  const [currentLat, setCurrentLat] = useState<number | undefined>();
+  const [currentLong, setCurrentLong] = useState<number | undefined>();
+
+  const incompatibleVersionLoaded = Boolean(
+    globalThis &&
+    globalThis.google?.maps?.version &&
+    !(
+      globalThis.google?.maps?.version.endsWith('-alpha') ||
+      globalThis.google?.maps?.version.endsWith('-beta')
+    )
+  );
+
+  if (incompatibleVersionLoaded) {
+    location.reload();
+    return;
+  }
+
+  const [selectedPlace, setSelectedPlace] = useState<google.maps.places.Place | null>(null);
+
+  // currnet latitude state setter
   const assignLat = async () => {
     try {
       const lat = await getLatitude()
@@ -20,6 +50,7 @@ export default function MapContent() {
     }
   }
 
+  // currnet longitude state setter
   const assignLong = async () => {
     try {
       const long = await getLongitude()
@@ -42,13 +73,22 @@ export default function MapContent() {
       <div className="map-content">
         {
           googleMapsApiKey ? (
-            <APIProvider apiKey={googleMapsApiKey!}>
+            <APIProvider apiKey={googleMapsApiKey!} version="beta">
               <Map
+                mapId={googleMapID}
                 gestureHandling={'greedy'}
                 zoom={18}
-                center={(currentLat && currentLong) ? { lat: currentLat, lng: currentLong } : { lat: 25.761663, lng: -80.191846 }}
+                defaultCenter={{ lat: 25.761663, lng: -80.191846 }}
                 className="h-140 w-full"
-              />
+              >
+                <AutocompleteControl
+                  controlPosition={ControlPosition.TOP_LEFT}
+                  selectedImplementation={selectedImplementation}
+                  onPlaceSelect={setSelectedPlace}
+                />
+
+                <AutocompleteResult place={selectedPlace} />
+              </Map>
             </APIProvider>
           ) : (
             <div className="">
---
+++ cash-near-app/components/react-google-maps/AutocompleteControl.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import React from 'react';
+import { ControlPosition, MapControl } from '@vis.gl/react-google-maps';
+
+import { AutocompleteCustom } from './AutocompleteCustom';
+import type { AutocompleteMode } from '../MapContent';
+
+type CustomAutocompleteControlProps = {
+  controlPosition: ControlPosition;
+  selectedImplementation: AutocompleteMode;
+  onPlaceSelect: (place: google.maps.places.Place | null) => void;
+};
+
+const AutocompleteControl = ({
+  controlPosition,
+  selectedImplementation,
+  onPlaceSelect
+}: CustomAutocompleteControlProps) => {
+  const { id } = selectedImplementation;
+
+  return (
+    <MapControl position={controlPosition}>
+      <div className="autocomplete-control">
+        {id === 'custom' && (
+          <AutocompleteCustom onPlaceSelect={onPlaceSelect} />
+        )}
+      </div>
+    </MapControl>
+  );
+};
+
+export default React.memo(AutocompleteControl);
