b54235f2cd38eb260ab51a30630a5e371c5012aa
Pablo <ortegaperaltap@gmail.com>
2025-09-13 22:38:40 -0400

    add api provider to MapContent component again, remove api wrapper from home page

--- cash-near-app/app/page.tsx
+++ cash-near-app/app/page.tsx
@@ -9,14 +9,12 @@ const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
 export default function Home() {
   return (
     <div className="body-content">
-      <APIProvider apiKey={googleMapsApiKey!}>
-        <Header />
-        <main className="px-4 py-4">
-          <section className="map">
-            <MapContent />
-          </section>
-        </main>
-      </APIProvider>
+      <Header />
+      <main className="px-4 py-4">
+        <section className="map">
+          <MapContent />
+        </section>
+      </main>
     </div>
   );
 }
