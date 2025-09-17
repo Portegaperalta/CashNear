05317646f20f73aee07c88449eb6f6b8555de4bd
Pablo <ortegaperaltap@gmail.com>
2025-09-16 21:40:19 -0400

    move header back to layout

--- cash-near-app/app/page.tsx
+++ cash-near-app/app/page.tsx
@@ -5,13 +5,10 @@ const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
 
 export default function Home() {
   return (
-    <div className="content">
-      <Header />
-      <main className="px-4 py-4">
-        <section className="map">
-          <MapContent googleMapsApiKey={googleMapsApiKey!} />
-        </section>
-      </main>
-    </div>
+    <main className="px-4 py-4">
+      <section className="map">
+        <MapContent googleMapsApiKey={googleMapsApiKey!} />
+      </section>
+    </main>
   );
 }
