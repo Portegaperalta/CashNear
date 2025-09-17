8c241c7569a6773c1e362e11fb30c154439c55ca
Pablo <ortegaperaltap@gmail.com>
2025-09-16 21:30:18 -0400

    change layout structure by moving header component to homepage

--- cash-near-app/app/layout.tsx
+++ cash-near-app/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -24,7 +23,6 @@ export default function RootLayout({
       <body
         className={`${inter.className}`}
       >
-        <Header />
         {children}
       </body>
     </html>
--- cash-near-app/app/page.tsx
+++ cash-near-app/app/page.tsx
@@ -1,13 +1,17 @@
+import Header from "@/components/Header";
 import MapContent from "@/components/MapContent";
 
 const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
 
 export default function Home() {
   return (
-    <main className="px-4 py-4">
-      <section className="map">
-        <MapContent googleMapsApiKey={googleMapsApiKey!} />
-      </section>
-    </main>
+    <div className="content">
+      <Header />
+      <main className="px-4 py-4">
+        <section className="map">
+          <MapContent googleMapsApiKey={googleMapsApiKey!} />
+        </section>
+      </main>
+    </div>
   );
 }
